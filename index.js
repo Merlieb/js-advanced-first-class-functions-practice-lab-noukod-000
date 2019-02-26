db9620e4f15c4d9b1bffef952b088af44823d6b9
Merline Belizaire <merline@noukod.com>
2019-02-26 14:32:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const drivers = [
+    { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+    { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+    { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+    { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+    { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+  ];
+
+const totalRevenue = function(drivers){
+  let totalPrice = 0;
+
+  drivers.forEach(function(driver){
+    totalPrice += driver.revenue;
+  })
+    return totalPrice
+}
+
+
+
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
+
+  averageRevenue(drivers)
